84284ca68fedbda9f2e1ff6aa02bac7b061cdf2b
Michael Holmes <michaelholmes618@gmail.com>
2021-04-10 13:15:11 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+//Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+const myName = "Mickey Holmes";
+const date = new Date();
+const CURRENTYEAR = date.getFullYear();
